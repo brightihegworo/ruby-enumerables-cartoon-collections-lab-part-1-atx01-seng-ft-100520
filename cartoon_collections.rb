c7605bfce01175d5ca54c70693fe8d94aa9b68a5
Bright It's <brightihegworo52@gmail.com>
2020-09-27 23:28:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |element, index|
-    print "#{index}. #{element}"
+    print "#{index + 1}. #{element}"
   end
 
 end
